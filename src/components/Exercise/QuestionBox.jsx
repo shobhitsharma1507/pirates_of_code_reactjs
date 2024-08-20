3f535363961c613fcb2ca79de3bb50fabeec83e8
sjatin-cse <sjatin.cse@gmail.com>
2024-08-20 14:38:28 +0530

    conflict in  quesBox

--- src/components/Exercise/QuestionBox.jsx
+++ src/components/Exercise/QuestionBox.jsx
@@ -1,16 +1,8 @@
-<<<<<<< Updated upstream
 import { Box } from "@mui/material";
 import QuestionBoxActions from "./QuestionBoxActions";
 import "./QuestionBox.css";
-import BooleanQues from "./BooleanQues";
-import McqQues from "./McqQues";
-=======
-import { Box } from '@mui/material'
-import QuestionBoxActions from './QuestionBoxActions'
-import './QuestionBox.css'
-import BooleanQues from './Types/BooleanQues'
-import McqQues from './Types/McqQues'
->>>>>>> Stashed changes
+import BooleanQues from "./Types/BooleanQues";
+import McqQues from "./Types/McqQues";
 import filterDark from './../../assets/images/quesFilterDark.svg'
 import filterLight from './../../assets/images/quesFilterLight.svg'
 import hintDark from './../../assets/images/quesHintDark.svg'
@@ -64,45 +56,45 @@ import BlankQues from './Types/BlankQues'
 // let tags = ['Basics', 'React', 'Intro']
 
 function QuestionBox({ type, qNo, ques, onSelectQuestion }) {
-    const [selectedAns, setSelectedAns] = useState([]);
-    const { theme, mode } = useThemeContext();
+  const [selectedAns, setSelectedAns] = useState([]);
+  const { theme, mode } = useThemeContext();
 
-    return (
-        <Box
-            className='quesbox-continer'
-            bgcolor='background.main'
-            color='text.main'
-        >
-            <Box className='flex-row questionBox-header'>
-                <img
-                    src={mode === 'dark' ? filterDark : filterLight}
-                    alt='filterIconDark'
-                ></img>
-            </Box>
-            <Box className='flex-col question-box-inner'>
-                <Box>
-                    {type === 'A' && <McqQues qNo={qNo + 1} ques={ques} handleAns={setSelectedAns} />}
-                    {type === 'B' && <BooleanQues qNo={qNo + 1} ques={ques} handleAns={setSelectedAns} />}
-                    {type === 'C' && <BlankQues qNo={qNo + 1} ques={ques} handleAns={setSelectedAns} />}
+  return (
+    <Box
+      className='quesbox-continer'
+      bgcolor='background.main'
+      color='text.main'
+    >
+      <Box className='flex-row questionBox-header'>
+        <img
+          src={mode === 'dark' ? filterDark : filterLight}
+          alt='filterIconDark'
+        ></img>
+      </Box>
+      <Box className='flex-col question-box-inner'>
+        <Box>
+          {type === 'A' && <McqQues qNo={qNo + 1} ques={ques} handleAns={setSelectedAns} />}
+          {type === 'B' && <BooleanQues qNo={qNo + 1} ques={ques} handleAns={setSelectedAns} />}
+          {type === 'C' && <BlankQues qNo={qNo + 1} ques={ques} handleAns={setSelectedAns} />}
 
-                </Box>
-            </Box>
-            <Box className='flex-row questionBox-filter'>
-                <img src={mode === "dark" ? hintDark : hintLight} alt='hintIcon'></img>
-            </Box>
-
-            <Box className='flex-row questionBox-tags' bgcolor='background.quesTag'>
-                <Box component='span' sx={{ display: 'inline' }}>
-                    Tags:
-                </Box>
-                {ques.tags.map((e, _) => (
-                    <QuesTag tagName={e} />
-                ))}
-            </Box>
-            <QuestionBoxActions ans={ques.answer} qNo={qNo} onSelectQuestion={onSelectQuestion} selectedAns={selectedAns} />
         </Box>
-    )
->>>>>>> Stashed changes
+      </Box>
+      <Box className='flex-row questionBox-filter'>
+        <img src={mode === "dark" ? hintDark : hintLight} alt='hintIcon'></img>
+      </Box>
+
+      <Box className='flex-row questionBox-tags' bgcolor='background.quesTag'>
+        <Box component='span' sx={{ display: 'inline' }}>
+          Tags:
+        </Box>
+        {ques.tags.map((e, _) => (
+          <QuesTag tagName={e} />
+        ))}
+      </Box>
+      <QuestionBoxActions ans={ques.answer} qNo={qNo} onSelectQuestion={onSelectQuestion} selectedAns={selectedAns} />
+    </Box>
+  )
+
 }
 
 export default QuestionBox;
