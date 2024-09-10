d3cd3a92de31e57b20ebdce7f3ea723b12a7516b
sjatin-cse <sjatin.cse@gmail.com>
2024-09-11 00:17:12 +0530

    Feat: style for tools landing page

--- src/pages/ToolsPage/ToolsPage.jsx
+++ src/pages/ToolsPage/ToolsPage.jsx
@@ -1,34 +1,136 @@
-import { Box } from '@mui/material'
-import React from 'react';
-import './ToolsPage.css'
+import { Box, Typography } from "@mui/material";
+import React, { useState } from "react";
+import "./ToolsPage.css";
+import Footer from "../../components/HomePage/Footer";
 
 export default function ToolsPage() {
-    const formatters = ["html", "css", "javascript", 'json', 'php', 'xml']
+    const formatters = ["html", "css", "javascript", "json", "php", "xml"];
+    const [isFormattersExpanded, setIsFormattersExpanded] = useState(false);
+    const [isConverterExpanded, setIsConverterExpanded] = useState(false);
+
     const converters = [
         { from: "json", to: "csv" },
         { from: "json", to: "excel" },
         { from: "json", to: "xml" },
         { from: "json", to: "yaml" },
-        { from: "", to: "" }]
+        { from: "", to: "" },
+    ];
 
-    return (<>
-        <Box>
-            toolspage
-
-            <Box className="flex-row formatter-tools">
-                Code Formatters
-                {formatters.map((e) => {
-                    return <Box className="formatter-tool-card"> {e.toUpperCase()} Beautifier </Box>
-                })}
-            </Box>
-            <Box className="flex-row conveter-tools">
-                Code conveters
-                {formatters.map((e) => {
-                    return <Box className="conveter-tool-card"> {e.toUpperCase()} Conveters </Box>
-                })}
+    return (
+        <>
+            <Box
+                className="flex-col"
+                sx={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                }}
+            >
+                <Box className="flex-row tools-page-title">
+                    <Typography variant="h4">Awesome Daily Tools</Typography>
                 </Box>
-        </Box>
-    </>
-
-    )
+                <Box
+                    className="flex-row tools-section"
+                    bgcolor="background.main"
+                >
+                    <Typography
+                        color="text.main"
+                        variant="h5"
+                        sx={{
+                            width: "100%",
+                            padding: "0 5%",
+                            textAlign: "left",
+                        }}
+                    >
+                        Code Formatters
+                    </Typography>
+                    <Box className="flex-row formatter-tools">
+                        {formatters
+                            .slice(
+                                0,
+                                isFormattersExpanded ? formatters.length : 2
+                            )
+                            .map((e) => {
+                                return (
+                                    <Box
+                                        className="flex-col formatter-tool-card"
+                                        bgcolor="background.sidebar"
+                                        color="text.main"
+                                        
+                                    >
+                                        {e.toUpperCase()} Beautifier
+                                    </Box>
+                                );
+                            })}
+                        <Box
+                            bgcolor="background.sidebar"
+                            color="text.main"
+                            className="flex-row formatter-tool-card"
+                            onClick={() =>
+                                setIsFormattersExpanded(!isFormattersExpanded)
+                            }
+                        >
+                            <Box className="flex-col tools-collapse-button">
+                                {isFormattersExpanded
+                                    ? "<< See Less"
+                                    : "See More >>"}
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>{" "}
+                <Box
+                    className="flex-row tools-section"
+                    bgcolor="background.main"
+                >
+                    <Typography
+                        color="text.main"
+                        variant="h5"
+                        sx={{
+                            width: "100%",
+                            padding: "0 5%",
+                            textAlign: "left",
+                        }}
+                    >
+                        Code Conveters
+                    </Typography>
+                    <Box className="flex-row conveter-tools">
+                        {converters
+                            .slice(
+                                0,
+                                isConverterExpanded ? converters.length : 2
+                            )
+                            .map((e) => {
+                                return (
+                                    <Box
+                                        className="flex-col conveter-tool-card"
+                                        bgcolor="background.sidebar"
+                                        color="text.main"
+                                    >
+                                        {e.from.toUpperCase() +
+                                            " to " +
+                                            e.to.toUpperCase()}{" "}
+                                        Conveter
+                                    </Box>
+                                );
+                            })}
+                        <Box
+                            className="flex-row formatter-tool-card"
+                            onClick={() =>
+                                setIsConverterExpanded(!isConverterExpanded)
+                            }
+                            bgcolor="background.sidebar"
+                            color="text.main"
+                        >
+                            <Box className="flex-col tools-collapse-button">
+                                {isConverterExpanded
+                                    ? "<< See Less"
+                                    : "See More >>"}
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+            <Footer />
+        </>
+    );
 }
